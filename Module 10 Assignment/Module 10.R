3d5e347ad7f757032b53a4d932156e9b6f5f2aa9
Benjamin Sajan <b.sajan228@gmail.com>
2022-05-05 08:27:32 -0400

    Module 10 added

---
+++ Module 10 Assignment/Module 10.R
@@ -0,0 +1,10 @@
+# Package: AstrophysicalEventMeasurers
+# Title: Contains formulas developed by physicists in astronomy 
+# Version: 0.1.0
+# Author: "Benjamin Sajan <sajanb@usf.edu> [aut, cre]"
+# Description: Contains formulas developed by physicists to determine 
+# survivability of life to a  cosmic disaster.
+# Contains functions used to calculate measures given planet data.
+# Depends: R (>= 3.1.2)
+# License: CC0
+  # LazyData: true
